2f5135e0e6bbe880d5fb13d3f47dcd7648f55433
ygneo <antonio.barcia@gmail.com>
2013-10-16 00:25:25 +0200

    Add static root setting

--- settings.py
+++ settings.py
@@ -48,7 +48,7 @@ USE_L10N = True
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = ''
 
-STATIC_ROOT = 'static/'
+STATIC_ROOT = '/var/www/som_website/static/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
