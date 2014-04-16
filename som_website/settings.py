14638e4ba2a89150368cdfec2b4967ea2863de5e
ygneo <antonio.barcia@gmail.com>
2014-04-16 16:38:27 +0200

    Add locale files

--- som_website/settings.py
+++ som_website/settings.py
@@ -82,13 +82,20 @@ DATABASES = {
 
 LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'Europe/Madrid'
+LANGUAGES = (
+  ('es', 'Español'),
+  ('en', 'English'),
+  ('ca', 'Català'),
+)
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
+
+TIME_ZONE = 'Europe/Madrid'
+
 
 
 # Static files (CSS, JavaScript, Images)
